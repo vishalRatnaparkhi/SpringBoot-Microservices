1f733edcc1b37180a9309eab7138583ab22cfdf0
Vishal Ratnaparkhi <ratnparkhiv@gmail.com>
2024-05-15 23:06:22 +0530

    changes related to Api Gateway

---
+++ src/main/java/com/spring/hotel/controlers/StaffController.java
@@ -0,0 +1,21 @@
+package com.spring.hotel.controlers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/staffs")
+public class StaffController {
+
+    @GetMapping
+    public ResponseEntity<List<String>> getStaffs() {
+        List<String> lists = Arrays.asList("Ram", "Kam", "Dam");
+        return new ResponseEntity(lists, HttpStatus.OK);
+    }
+}
